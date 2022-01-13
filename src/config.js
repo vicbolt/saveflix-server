ed5fbfa6291bdb3be753f3cc3d335519e5e3ba4e
vicbolt <vicbolt.madrid@gmail.com>
2022-01-13 10:40:54 +0100

    hostaname

--- src/config.js
+++ src/config.js
@@ -15,7 +15,7 @@ const config = {
     jwt: {
         secret: '01001000100001'
     },
-    hostname: "http://localhost:4500/",
+    hostname: "https://saveflix-client.herokuapp.com/",
     imageFolder: './src/public',
 };
 
