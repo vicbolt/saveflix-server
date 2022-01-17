17c87760b2540b893b92b9267904bcb1366078db
vicbolt <vicbolt.madrid@gmail.com>
2022-01-17 21:14:57 +0100

    server 3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+
 const app = express();
+
 const http = require('http');
+
 const server = http.createServer(app);
+
 const { Server } = require("socket.io");
 const io = new Server(server);
 
@@ -10,9 +14,10 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log('nuevo usuario conectado');
 });
 
 server.listen(process.env.PORT || 4500, () => {
     console.log('Server is running on port: 4500');
   });
+
