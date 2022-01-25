d40ec41d6a6b93a483af5f5b943dbe6dac80b4f4
vicbolt <vicbolt.madrid@gmail.com>
2022-01-25 11:55:13 +0100

    prueba NF servidor

--- src/index.js
+++ src/index.js
@@ -3,28 +3,10 @@ require("dotenv").config()
 require('./database')
 
 
-const server = require("./server")
-const app = require('express')();
-const http = require('http').createServer(app);
+const server = require('./server');
 
-const io = require('socket.io')(http, {
-    cors: {
-      origins: ['http://localhost:3000']
-    }
-  });
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hey Socket.io</h1>');
+server.listen(server.get('PORT'), () =>{
+  console.log('Server is running on port ', server.get('PORT'))
 });
 
-io.on('connection', (socket) => {
-    console.log('A user connected');
-
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
-  });
-
-  http.listen(server.get("PORT"), () => {
-    console.log('listeninghttp on: ', server.get("PORT"));
-  });
+module.exports = server
